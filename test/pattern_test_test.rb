61886f961930d8be2847a932c5be03840ce03e76
Soutaro Matsumoto <matsumoto@soutaro.com>
2016-09-13 22:15:13 +0900

    Refactor pattern_test_test

--- test/pattern_test_test.rb
+++ test/pattern_test_test.rb
@@ -3,16 +3,25 @@ require_relative "test_helper"
 class PatternTestTest < Minitest::Test
   include TestHelper
 
+  def assert_node(node, type:)
+    refute_nil node
+    assert_equal type, node.type
+    yield node.children if block_given?
+  end
+
   def test_ivar_without_name
     nodes = query_pattern("@", "@x.foo")
     assert_equal 1, nodes.size
-    assert_equal :ivar, nodes.first.type
-    assert_equal :@x, nodes.first.children.first
+    assert_node nodes.first, type: :ivar do |name, *_|
+      assert_equal :@x, name
+    end
   end
 
   def test_ivar_with_name
     nodes = query_pattern("@x", "@x + @y")
     assert_equal 1, nodes.size
-    assert_equal :@x, nodes.first.children.first
+    assert_node nodes.first, type: :ivar do |name, *_|
+      assert_equal :@x, name
+    end
   end
 end
