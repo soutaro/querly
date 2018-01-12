79f8269e3f97c50dfb30ff5d771d5e110d518301
kotaro.nishi <kotaro.nishi@speee.jp>
2018-01-12 18:23:23 +0900

    Fix namespace of ::Concerns

--- lib/querly/concerns/backtrace_formatter.rb
+++ lib/querly/concerns/backtrace_formatter.rb
@@ -1,7 +1,9 @@
-module Concerns
-  module BacktraceFormatter
-    def format_backtrace(backtrace, indent: 2)
-      backtrace.map {|x| " "*indent + x }.join("\n")
+module Querly
+  module Concerns
+    module BacktraceFormatter
+      def format_backtrace(backtrace, indent: 2)
+        backtrace.map {|x| " "*indent + x }.join("\n")
+      end
     end
   end
 end
